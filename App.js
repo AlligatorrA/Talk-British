c59d974832cbfa6a1a03eae1ccf293f728609605
Anand <anandraj143kick@gmail.com>
2021-07-07 17:48:36 +0530

    talk british
    Done

---
+++ App.js
@@ -0,0 +1,39 @@
+var translate = document.querySelector("#translate");
+var txt = document.querySelector("#txt");
+var output = document.querySelector("#output");
+
+
+const serverURL = "https://api.funtranslations.com/translate/us2uk.json"
+
+function getTranslationURL(text){
+  return serverURL + "?" + "text=" + text
+}
+
+function erorHandler(){
+  console.log("error occured", error );
+  alert(" 😔 Error mate! Try after some time 😔");
+}
+
+function clickHaandler(){
+  var inputText = txt.value;
+  output.innerHTML = (inputText);
+
+fetch(getTranslationURL(inputText))
+.then(response => response.json())
+.then(json => {
+  var translatedText = json.content.translated;
+  output.innerHTML = translatedText;
+})
+
+};
+
+
+
+
+function speakHandler(){
+    const out = document.getElementById("output").value
+    responsiveVoice.speak(out);
+   
+  }
+translate.addEventListener("click",clickHaandler);
+speak.addEventListener("click", speakHandler);
